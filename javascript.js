96f881fa82bdd64bd219d1e8cf5af920a7c15208
Josh <joshua_litam@yahoo.com>
2022-03-23 11:25:18 +0800

    Add comments and labels

--- javascript.js
+++ javascript.js
@@ -1,12 +1,39 @@
+// global variables
+let containerWidth = 400;
+let containerHeight = 400;
+let currentRow = 16;
+let currentColumn = 16;
 
-let containerWidth = 1000;
-let containerHeight = 1000;
-
+// primary container
 let divContainer = document.createElement('div');
-divContainer.style.border = "thick solid red";
-divContainer.style.width = containerWidth;
-divContainer.style.height = containerHeight;
-divContainer.className = "div-container"
+divContainer.className = "div-container";
+
+// labels wrapper
+let labelWrapper = document.createElement('div');
+labelWrapper.className = "label-wrapper";
+
+let titleParagraph = document.createElement('p');
+titleParagraph.innerHTML = "Etch a Sketch";
+titleParagraph.className = "title-paragraph";
+
+let authorParagraph = document.createElement('p');
+authorParagraph.innerHTML = "By: Josh";
+authorParagraph.className = "author-paragraph";
+
+labelWrapper.appendChild(titleParagraph);
+labelWrapper.appendChild(authorParagraph);
+
+// sketch wrapper
+let sketchWrapper = document.createElement('div');
+sketchWrapper.className = "sketch-wrapper";
+
+let pixelCount = document.createElement('div');
+pixelCount.innerHTML = "Current pixel size is: " + currentRow + "x" + currentColumn;
+pixelCount.className = "pixel-count"
+
+let sketchContainer = document.createElement('div');
+sketchContainer.style.border = "thick solid black";
+sketchContainer.className = "sketch-container"
 
 let resetButton = document.createElement('button');
 resetButton.innerHTML = "Reset";
@@ -15,7 +42,13 @@ resetButton.addEventListener("click", () => {
     askNumber()
 })
 
-document.body.appendChild(resetButton);
+sketchWrapper.appendChild(pixelCount);
+sketchWrapper.appendChild(sketchContainer);
+sketchWrapper.appendChild(resetButton);
+
+// appending on documents
+divContainer.appendChild(labelWrapper);
+divContainer.appendChild(sketchWrapper)
 document.body.appendChild(divContainer);
 
 function askNumber() {
@@ -23,6 +56,9 @@ function askNumber() {
     let column = prompt("Please enter the number of columns", "Maximum of 100")
     if (row <= 100 && column <= 100) {
         createGrid(row, column)
+        currentRow = row;
+        currentColumn = column;
+        pixelCount.innerHTML = "Current pixel size is: " + currentRow + "x" + currentColumn;
     } else {
         errorMessage()
     }
@@ -34,7 +70,7 @@ function errorMessage() {
 }
 
 function createGrid(row, column) {
-    deleteChildren(divContainer)
+    deleteChildren(sketchContainer)
     let numberOfSquares = row * column
     for (let i = 0; i < numberOfSquares; i++) {
         let div = document.createElement('div');
@@ -49,7 +85,7 @@ function createGrid(row, column) {
             div.style.opacity = `${opacity}`
             opacity += .1
         })
-        divContainer.appendChild(div);
+        sketchContainer.appendChild(div);
     }
 }
 
